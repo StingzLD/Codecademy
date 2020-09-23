bc3313bca1e140d7bcdeee888c2606ecca0e6ca6
Corey Hastings <47801434+StingzLD@users.noreply.github.com>
2020-09-22 21:43:34 -0500

    Update getting-ready-for-physics-class.py

--- Python3/Freeform_Projects/2_Functions/getting-ready-for-physics-class.py
+++ Python3/Freeform_Projects/2_Functions/getting-ready-for-physics-class.py
@@ -1,55 +1,37 @@
 # Turn up the Temperature
-#1
 def f_to_c(f_temp):
-  c_temp = (f_temp - 32) * 5 / 9
+  c_temp = (f_temp - 32) * (5/9)
   return c_temp
 
-#3
 def c_to_f(c_temp):
-  f_temp = c_temp * (9 / 5) + 32
+  f_temp = c_temp * (9/5) + 32
   return f_temp
 
-#2
 f100_in_celsius = f_to_c(100)
-print("100 *F = " + str(f100_in_celsius) + " *C")
-
-#4
+print(f100_in_celsius)
 c0_in_fahrenheit = c_to_f(0)
-print("0 *C = " + str(c0_in_fahrenheit) + " *F \n")
+print(c0_in_fahrenheit)
 
 # Use the Force
+def get_force(mass,acceleration):
+  return mass * acceleration
+
+def get_energy(mass,c=3*10**8):
+  return mass * c
+
 train_mass = 22680
-train_accel = 10
-train_dist = 100
+train_acceleration = 10
+train_distance = 100
 bomb_mass = 1
 
-#5
-def get_force(mass, accel):
-  force = mass * accel
-  return force
-
-#8
-def get_energy(mass, c = 3 * 10 ** 8):
-  energy = mass * c ** 2
-  return energy
-
-#6-7
-train_force = get_force(train_mass, train_accel)
-print("The GE train supplies " + str(train_force) + " Newtons of force. 
-\n")
-
-#9-10
-bomb_energy = get_energy (bomb_mass)
-print("A 1kg bomb supplies " + str(bomb_energy) + " Joules. \n")
+train_force = get_force(train_mass,train_acceleration)
+print("The GE train supplies "+str(train_force)+" Newtons of force.")
+bomb_energy = get_energy(bomb_mass)
+print("A 1kg bomb supplies "+str(bomb_energy)+" Joules of energy.")
 
 # Do the Work
-#11
-def get_work(mass, accel, dist):
-  force = get_force(mass, accel) * dist
-  return force
+def get_work(mass,acceleration,distance):
+  return get_force(mass,acceleration) * distance
 
-#12-13
-train_work = get_work(train_mass, train_accel, train_dist)
-print("The GE train does " + str(train_work) + " Joules of work over" + 
-str(train_dist) + " meters.")
-
+train_work = get_work(train_mass,train_acceleration,train_distance)
+print("The GE train does "+str(train_work)+" Joules of work over "+str(train_distance)+" meters.")
