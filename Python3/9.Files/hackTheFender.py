49c816272dbbd34d4ba52ed8140e44fc95e8ece2
StingzLD <stingzlightingdesign@hotmail.com>
2021-03-18 16:47:40 -0500

    Updated hackTheFender.py formatting

--- Python3/9.Files/hackTheFender.py
+++ Python3/9.Files/hackTheFender.py
@@ -1,34 +1,32 @@
+import json
 import csv
 
 compromised_users = []
 
 # Pull all usernames from 'passwords.csv'
 with open('passwords.csv') as password_file:
-  password_csv = csv.DictReader(password_file)
-  for row in password_csv:
-    compromised_users.append(row['Username'])
-#print(compromised_users)
+    password_csv = csv.DictReader(password_file)
+    for row in password_csv:
+        compromised_users.append(row['Username'])
+# print(compromised_users)
 
 # Create a new file consisting of the usernames pulled
 with open('compromised_users.txt', 'w') as compromised_user_file:
-  for user in compromised_users:
-    compromised_user_file.write(user)
+    for user in compromised_users:
+        compromised_user_file.write(user)
 
-
-
-import json
 
 # Create a message in JSON from Python
 with open('boss_message.json', 'w') as boss_message:
-  boss_message_dict = {
-    'recipient':'The Boss',
-    'message':'Mission Success'
-  }
-  json.dump(boss_message_dict, boss_message)
+    boss_message_dict = {
+        'recipient': 'The Boss',
+        'message': 'Mission Success'
+    }
+    json.dump(boss_message_dict, boss_message)
 
 # Create file to replace 'passwords.csv'
 with open('new_passwords.csv', 'w') as new_passwords_obj:
-  slash_null_sig = '''
+    slash_null_sig = '''
    _  _     ___   __  ____             
   / )( \   / __) /  \(_  _)            
   ) \/ (  ( (_ \(  O ) )(              
@@ -46,6 +44,6 @@ with open('new_passwords.csv', 'w') as new_passwords_obj:
   /    /) \/ (/ (_/\/ (_/\             
   \_)__)\____/\____/\____/
   '''
-  new_passwords_obj.write(slash_null_sig)
+    new_passwords_obj.write(slash_null_sig)
 
 # Codecademy will "take care of moving the new passwords file over"
